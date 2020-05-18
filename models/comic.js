089f35fd5826669b5c839119949e8788af5294aa
david <taweihuangdavid@gmail.com>
2020-05-18 10:21:55 +0800

    project name modification
    build comics api

---
+++ models/comic.js
@@ -0,0 +1,46 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const { genreSchema } = require("./genre");
+
+const Comic = mongoose.model(
+  "Comics",
+  new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    genre: {
+      type: genreSchema,
+      required: true,
+    },
+    numberInStock: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255,
+    },
+    dailyRentalRate: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255,
+    },
+  })
+);
+
+function validateComic(comic) {
+  const schema = {
+    title: Joi.string().min(5).max(50).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().min(0).required(),
+    dailyRentalRate: Joi.number().min(0).required(),
+  };
+
+  return Joi.validate(comic, schema);
+}
+
+exports.Comic = Comic;
+exports.validate = validateComic;
---
+++ routes/comics.js
@@ -0,0 +1,78 @@
+const { Comic, validate } = require("../models/comic");
+const { Genre } = require("../models/genre");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const comics = await Comic.find().sort("name");
+  res.send(comics);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
+
+  let comic = new Comic({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  comic = await comic.save();
+
+  res.send(comic);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre.");
+
+  const comic = await Comic.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+    },
+    { new: true }
+  );
+
+  if (!comic)
+    return res.status(404).send("The comic with the given ID was not found.");
+
+  res.send(comic);
+});
+
+router.delete("/:id", async (req, res) => {
+  const comic = await Comic.findByIdAndRemove(req.params.id);
+
+  if (!comic)
+    return res.status(404).send("The comic with the given ID was not found.");
+
+  res.send(comic);
+});
+
+router.get("/:id", async (req, res) => {
+  const comic = await Comic.findById(req.params.id);
+
+  if (!comic)
+    return res.status(404).send("The comic with the given ID was not found.");
+
+  res.send(comic);
+});
+
+module.exports = router;
