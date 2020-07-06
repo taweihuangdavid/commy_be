de26f913b0ad7ade579491b7612e4cd68a2c7a9b
david <taweihuangdavid@gmail.com>
2020-07-06 23:05:54 +0800

    remove helmet package from npm

--- startup/prod.js
+++ startup/prod.js
@@ -1,8 +1,8 @@
 // All the middlewares we need for production environment
-const helmet = require("helemt");
+//const helmet = require("helemt");
 const compression = require("compression");
 
 module.exports = function (app) {
-  app.use(helmet());
+  //app.use(helmet());
   app.use(compression());
 };
