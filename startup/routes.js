f546859c404755f300e72a1a0582e4b4d6513b7f
david <taweihuangdavid@gmail.com>
2020-07-07 09:41:50 +0800

    adding CORS headers

--- startup/routes.js
+++ startup/routes.js
@@ -10,6 +10,14 @@ const error = require("../middleware/error");
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
   app.use("/api/comics", comics);
