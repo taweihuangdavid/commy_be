01f04f576625b3ed381b2a7e39b7a750564c531d
david <taweihuangdavid@gmail.com>
2020-07-07 09:54:50 +0800

    CORS headers modifications 2

--- startup/routes.js
+++ startup/routes.js
@@ -11,10 +11,7 @@ const error = require("../middleware/error");
 module.exports = function (app) {
   app.use(express.json());
   app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://fast-retreat-54128.herokuapp.com"
-    ); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "*"); // update to match the domain you will make the request from
     next();
   });
   app.use("/api/genres", genres);
