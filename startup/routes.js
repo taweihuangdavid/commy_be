d8fa4af524b3e5c23094aa8c0430db10893e7112
david <taweihuangdavid@gmail.com>
2020-07-07 09:43:58 +0800

    adding CORS domain

--- startup/routes.js
+++ startup/routes.js
@@ -11,7 +11,10 @@ const error = require("../middleware/error");
 module.exports = function (app) {
   app.use(express.json());
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://fast-retreat-54128.herokuapp.com/comics"
+    ); // update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
